8772df5a9247d596fa112d09dfa2777122c8b93e
MikiKentaro <ken6131xperia@gmail.com>
2015-04-07 01:22:28 +0900

    part2

--- ajaxTest/public/javascripts/task.js
+++ ajaxTest/public/javascripts/task.js
@@ -1,225 +1,256 @@
 ﻿// ページが表示されたときToDoリストを表示する
-$(function(){
-  getList();
-  wantload();
+$(function() {
+    getList();
+    wantload();
 });
-
 // フォームを送信ボタンを押すと、ToDoを追加して再表示する。
-$('#form').submit(function(){
-  postList();
-  return false;
+$('#form').submit(function() {
+    postList();
+    return false;
 });
-
-$('#wanton').click(function(){
-  wantswich();
+$('#wanton').click(function() {
+    wantswich();
 });
 var wantbool;
 
-function wantload(){
-wantbool=false;
-document.getElementById("wanttext").innerHTML = '<div>Have To Do</div>';
+function wantload() {
+        wantbool = false;
+        document.getElementById("wanttext").innerHTML = '<div>Have To Do</div>';
+    }
+    // ToDo一覧を取得して表示する
 
-}
-// ToDo一覧を取得して表示する
-function getList(){
-var count;
-count=0;
+function getList() {
+        var count;
+        count = 0;
+        var totalcount;
+        totalcount = 0;
+        var gro;
+        gro = sessionStorage.getItem("tasknum");
+        //alert(gro);
+        var groupname;
+        groupname = gro;
+        var num;
+        num = 0;
+        document.getElementById("todoname").innerHTML = groupname + '<br>';
+        // すでに表示されている一覧を非表示にして削除する
+        var $list = $('.list');
+        $list.fadeOut(function() {
+            $list.children().remove();
+            // /todoにGETアクセスする
+            $.get('todo', function(todos) {
+                // 取得したToDoを追加していく
+                $.each(todos, function(index, todo) {
+                    if (todo.group == groupname && todo
+                        .isTitles == false) {
+                        var create = new Date(todo.createdDate);
+                        var credate = create.getDate();
+                        var cremonth = create.getMonth() +
+                            1;
+                        var creyear = create.getFullYear();
+                        var limit = new Date(todo.limitDate);
+                        var mydate = limit.getDate();
+                        var mymonth = limit.getMonth() +
+                            1;
+                        var myyear = limit.getFullYear();
+                        var checkclor;
+                        var isFinish;
+                        if (todo.wanthave == false) {
+                            checkcolor = "#ddddff";
+                            if (todo.isCheck == true) {
+                                isFinish = "完了";
+                            } else {
+                                isFinish = "未完了";
+                            }
+                        } else {
+                            if (todo.isCheck == true) {
+                                checkcolor = "#ddffdd";
+                                isFinish = "完了";
+                            } else {
+                                checkcolor = "#ffdddd";
+                                isFinish = "未完了";
+                            }
+                        }
+                        $list.append(
+                            '<div class="tasktodo" style="background-color:' +
+                            checkcolor +
+                            '"><div class=check2' +
+                            num +
+                            '><div class="tasktitle">' +
+                            todo.text + ' </div>' +
+                            '<div> 作成日' + creyear +
+                            '/' + cremonth + '/' +
+                            credate + '   期限' +
+                            myyear + '/' + mymonth +
+                            '/' + mydate +
+                            '</div></div>' +
+                            '<div class="taskdeleat"><input id="deleatButton' +
+                            num +
+                            '" type="button" value="×"></div>' +
+                            '</div><div class="isfinish">' +
+                            isFinish + '</div>');
+                        $list.append(
+                            '<script>$("#deleatButton' +
+                            num +
+                            '").click(function() { deleatbutton' +
+                            num + '();});' +
+                            'function deleatbutton' +
+                            num + '() {  ' +
+                            'var deleattask = "' +
+                            todo.text + '";' +
+                            'var deleatgru="' +
+                            todo.group + '";' +
+                            '$.post("/todo", {taskname2: deleattask,delgru:deleatgru}, function(res){console.log(res);' +
+                            'getList();' + '});' +
+                            //'alert("asdd");'+
+                            '}</script>');
+                        $list.append(
+                            '<script>$(".check2' +
+                            num +
+                            '").click(function() { checkbox' +
+                            num + '();countcheck' +
+                            num + '();});' +
+                            'function checkbox' +
+                            num + '() {  ' +
+                            'var checknum = "' +
+                            todo.text + '";' +
+                            'var checkonoff="' +
+                            todo.isCheck + '";' +
+                            'var checkgroup="' +
+                            todo.group + '";' +
+                            // 'alert("asdd");'+
+                            '$.post("/todo", {checktask: checknum,taskbool:checkonoff,taskgroup:checkgroup}, function(res){console.log(res);' +
+                            'getList();' + '});' +
+                            '}' +
+                            'function countcheck' +
+                            num + '() {  ' +
+                            'var checkgroup="' +
+                            todo.group + '";' +
+                            '$.post("/todo", {taskgroup2:checkgroup}, function(res){console.log(res);' +
+                            '});' + '}</script>');
+                        num += 1;
+                    }
+                });
+                if (num == 0) {
+                    document.getElementById("todocom").innerHTML =
+                        "<div class='errtext'>Todoが作成されていません</div>";
+                } else {}
+                // 一覧を表示する
+                $list.fadeIn();
+            });
+        });
+    }
+    // フォームに入力されたToDoを追加する
 
-var totalcount;
-totalcount=0;
-
-
-
-
-var gro;
-gro = sessionStorage.getItem("tasknum");
-//alert(gro);
-var groupname;
-groupname=gro;
-
-var num;
-num=0;
-
-	  document.getElementById("todoname").innerHTML = groupname+'<br>';
-
-  // すでに表示されている一覧を非表示にして削除する
-  var $list = $('.list');
-  $list.fadeOut(function(){
-    $list.children().remove();
-    // /todoにGETアクセスする
-    $.get('todo', function(todos){
-      // 取得したToDoを追加していく
-	 
-      $.each(todos, function(index, todo){
-	  
-	 
-	  if(todo.group==groupname&&todo.isTitles==false){
-	  
-	  var create=new Date(todo.createdDate);
-	 var credate=create.getDate();
-	  var cremonth=create.getMonth()+1;
-    var creyear=create.getFullYear();
-	
-	  
-        var limit = new Date(todo.limitDate);
-		var mydate=limit.getDate();
-  var mymonth=limit.getMonth()+1;
-    var myyear=limit.getFullYear();
-	
-	var checkclor;
-	var isFinish;
-	
-	if(todo.wanthave==false){
-	checkcolor="#ddddff";
-	if(todo.isCheck==true){
-	isFinish="完了";
-	}else{
-	isFinish="未完了";
-	}
-	}
-	else{
-	if(todo.isCheck==true){
-	checkcolor="#ddffdd";
-	isFinish="完了";
-	}else{
-	checkcolor="#ffdddd";
-	isFinish="未完了";
-
-	}
-	}
-	
-	$list.append('<div class="tasktodo" style="background-color:'+checkcolor+'"><div class=check2'+num+'><div class="tasktitle">'+todo.text+'<input type="checkbox" id="check'+num+'" ' + (todo.isCheck ? 'checked' : '') + '>'+' </div>'+
-        '<div> 作成日' +creyear+'/'+cremonth+'/'+credate + '   期限' +myyear+'/'+mymonth+'/'+mydate + '</div></div>'+
+function postList() {
+        var groupname;
+        groupname = sessionStorage.getItem("tasknum");
+        //alert(wantbool);
+        // フォームに入力された値を取得
+        var name = $('#text').val();
+        var limitDate = new Date($('#limit').val());
+        var istitle = false;
 		
-		'<div class="taskdeleat"><input id="deleatButton'+num+'" type="button" value="×"></div>'+
-		'</div><div class="isfinish">'+isFinish+'</div>');
-	
-		
-		
-		$list.append('<script>$("#deleatButton'+num+'").click(function() { deleatbutton'+num+'();});'+
-'function deleatbutton'+num+'() {  '+
- 'var deleattask = "'+todo.text+'";'+
- 'var deleatgru="'+todo.group+'";'+
-'$.post("/todo", {taskname2: deleattask,delgru:deleatgru}, function(res){console.log(res);'+
-'getList();'+
-'});'+
-//'alert("asdd");'+
-'}</script>');
-
-$list.append('<script>$(".check2'+num+'").click(function() { checkbox'+num+'();countcheck'+num+'();});'+
-'function checkbox'+num+'() {  '+
- 'var checknum = "'+todo.text+'";'+
- 'var checkonoff="'+todo.isCheck+'";'+
- 'var checkgroup="'+todo.group+'";'+
-// 'alert("asdd");'+
-'$.post("/todo", {checktask: checknum,taskbool:checkonoff,taskgroup:checkgroup}, function(res){console.log(res);'+
-
-'getList();'+
-'});'+
-
-'}'+
-'function countcheck'+num+'() {  '+
- 'var checkgroup="'+todo.group+'";'+
-
-'$.post("/todo", {taskgroup2:checkgroup}, function(res){console.log(res);'+
-'});'+		
-'}</script>');
-		
-		
-
-	num+=1;	
-		
-
-		}
-	
+		//var val=escapeText(name());
 		/*
-		totalcount+=1;
+		if(checkText(val)){
 		
-		if(todo.isCheck="true"){
-		count+=1;
+		alert("bbb");
+		
+		
 		}
-		$list.append('<div>'+totalcount+'個中'+count+'個クリア</div>');
 		*/
-      });
-      // 一覧を表示する
-      $list.fadeIn();
-    });
-  });
+		var notword;
+		notword=">";
+		
+		var notword2;
+		notword2="<";
+		
+		var notword3;
+		notword3="&";
+		//alert(name);
+	//	alert(escape(val));
+        //名前が未入力のとき
+        //if (name != ""|| name.indexOf(notword) ==　-1|| name.indexOf(">")!=　-1) {
 
-
-
-
-}
-
-// フォームに入力されたToDoを追加する
-function postList(){
-
-var groupname;
-groupname = sessionStorage.getItem("tasknum");
-
-alert(wantbool);
-  // フォームに入力された値を取得
-  var name = $('#text').val();
-  var limitDate = new Date($('#limit').val());
-  var istitle=false;
- 
-// if(name==""){
- //alert("axss");
-
- //}
- if(name!=""){
- //alert("nuuu");
- 
-  //入力項目を空にする
-  $('#text').val('');
-  $('#limit').val('');
-
-  // /todoにPOSTアクセスする
-  $.post('/todo', {name: name, limit: limitDate,isTitles:istitle, group: groupname,wanton:wantbool}, function(res){
-    console.log(res);
-    //再度表示する
-    getList();
-  });
-  
-  }
-  
-}
-
-function wantswich(){
-
-if(wantbool==false){
-wantbool=true;
-//alert("buu");
-//document.getElementById("wanton").innerHTML = '<div><input type="checkbox" ' + (wantbool ? 'checked' : '') + '>want to do</div>';
-document.getElementById("wanttext").innerHTML = '<div> Want To Do</div>';
-
-}else{
-wantbool=false;
-document.getElementById("wanttext").innerHTML = '<div>Have To Do</div>';
-
-}
+		    //alert("nuuu");
+            //入力項目を空にする
+            $('#text').val('');
+            $('#limit').val('');
+			
+		if(name.indexOf(notword) !=　-1){
+		 //alert("1文字以上入力してください");
+		 document.getElementById("todocom").innerHTML ="<div class='errtext'><>&の使用は控えてください" + '<br></div>';
+		 return;
+		}else if(name.indexOf(notword2) !=　-1){
+		 document.getElementById("todocom").innerHTML ="<div class='errtext'><>&の使用は控えてください" + '<br></div>';
+		 return;
+		}else if(name.indexOf(notword3) !=　-1) {
+		 document.getElementById("todocom").innerHTML ="<div class='errtext'><>&の使用は控えてください" + '<br></div>';
+		 return;
+}else if(name==""){
+		 document.getElementById("todocom").innerHTML ="<div class='errtext'>テキストフォームに何も書かれていません" + '<br></div>';
+		 return;
 
 
 }
 
+            // /todoにPOSTアクセスする
+            $.post('/todo', {
+                name: name,
+                limit: limitDate,
+                isTitles: istitle,
+                group: groupname,
+                wanton: wantbool
+            }, function(res) {
+                console.log(res);
+                //再度表示する
+                document.getElementById("todocom").innerHTML =
+                    "新しいToDoが追加されました" + '<br>';
+                getList();
+            });
+			
+        //} else {
+        //    alert("1文字以上入力してください");
+        //}
+		
+    }
+    //Todoの切り替えボタンを押したとき
 
-/*
+function wantswich() {
+        if (wantbool == false) {
+            wantbool = true;
+            document.getElementById("wanttext").innerHTML =
+                '<div> Want To Do</div>';
+        } else {
+            wantbool = false;
+            document.getElementById("wanttext").innerHTML =
+                '<div>Have To Do</div>';
+        }
+    }
+    
 
-function escapeText(text) {
-  return $("<div>").text(text).html();
+function escapeText(name) {
+  return $("<div>").text(name).html();
+  //alert("aaa");
 }
 
 // 入力チェックを行う
-function checkText() {
+function checkText(name) {
   // 文字数が0または20以上は不可
+  //alert("aaa");
+  if(name.indexOf("a")!=　-1||name.indexOf("b")!=　-1){
+   alert("<>の記号の使用は避けてください");
+    return false;
+  }
   
+  /*
   
   if(text.indexOf("&lt;")!=　-1||text.indexOf("&gt;")!=　-1){
    alert("<>の記号の使用は避けてください");
     return false;
   }
-
- 
+*/
+ /*
   // すでに入力された値があれば不可
   var length = localStorage.length;
   for (var i = 0; i < length; i++) {
@@ -231,12 +262,12 @@ function checkText() {
       return false;
     }
   }
-
+*/
   // すべてのチェックを通過できれば可
   return true;
 }
-*/
-/*
+
+    /*
 if(checkText(val)) {
 
 //var str=JSON.stringify(todo);
--- ajaxTest/public/javascripts/todo.js
+++ ajaxTest/public/javascripts/todo.js
@@ -1,185 +1,204 @@
 ﻿// ページが表示されたときToDoリストを表示する
-$(function(){
-  getList();
- 
-});
-
-// フォームを送信ボタンを押すと、ToDoを追加して再表示する。
-$('#form').submit(function(){
-  postList();
-  return false;
-});
-
-
-
-
-// ToDo一覧を取得して表示する
-function getList(){
-
-
-var smp;
-
-
-var num;
-num=0;
-
-
-  // すでに表示されている一覧を非表示にして削除する
-  var $list = $('.list');
-  $list.fadeOut(function(){
-    $list.children().remove();
-    // /todoにGETアクセスする
-    $.get('todo', function(todos){
-      // 取得したToDoを追加していく
-	  // var ab=db.todo.find();
-	 // alert(ab);
-	
-	  
-      $.each(todos, function(index, todo){
-	  
-	  
-	 if(todo.isTitles==true){
-	  //alert('omu');
-	  //smp=todo.name.count();
-	  
-  	
-
-	  
-	  
-	  var create=new Date(todo.createdDate);
-	 var credate=create.getDate();
-	  var cremonth=create.getMonth()+1;
-    var creyear=create.getFullYear();
-	
-   
-	  
-        var limit = new Date(todo.limitDate);
-		var mydate=limit.getDate();
-  var mymonth=limit.getMonth()+1;
-    var myyear=limit.getFullYear();
-	
-// $.post('/todo', {groupname: todo.group}, function(res){
-//   console.log(res);
-//  });
-	
-	if(todo.taskcount!=0){
-	var parsent=todo.finished/todo.taskcount*100;
-	 
-	var parsent2=Math.round(parsent);
-	
-	
-	
-	
-	
-	
-	var setgroup;
-	setgroup='aa';
-	
-		$list.append('<div class="deleat"><input id="deleatButton'+num+'" type="button" value="×"></div>'+
-
-	'<div class="todotodo"><div class="linktodo'+num+'"><a href="/task" style="text-decoration:none;"><div class="title">'+todo.group+'</div>'+
-
-
-	
-	'<div class="todo"><div><div class="par">達成率'+parsent2+'％</div><div class=parbar><img src="images/lightgreen.jpg" width="'+parsent2+'%" height="30"></div></div>'+
-	
-	'<div>'+todo.taskcount+'個中'+todo.finished+'個チェック済み'+
-//	'<input type="checkbox" id="check'+num+'" ' + (todo.isCheck ? 'checked' : '') + '>'+
-         ' 作成日：' +creyear+'/'+cremonth+'/'+credate + '  期限：' +myyear+'/'+mymonth+'/'+mydate + '</div>'+
-	//$list.append('<script>$("#check").click(function(){checkbox1();});function checkbox1(){ $.get("/todo", function(todos){ $.each(todos, function(index, todo){todo.isCheck="true";});});}</script>');
-    //'<div><a href="/task"><input id="linkButton'+num+'" type="button" value="詳細"></a></div>'+
-	'</div></div></div>');
-
-
-$list.append('<script>$(".linktodo'+num+'").click(function() { editbutton'+num+'(); });'+'function editbutton'+num+'() { sessionStorage.setItem("tasknum","'+todo.group+'");}</script>');
-$list.append('<script>$("#deleatButton'+num+'").click(function() { deleatbutton'+num+'();});'+
-'function deleatbutton'+num+'() {  '+
- 'var deleatss = "'+todo.group+'";'+
-// 'alert("asdd");'+
-'$.post("/todo", {groupname2: deleatss}, function(res){console.log(res);'+
-'getList();'+
-'});'+
-'}</script>');
-/*
-$list.append('<script>$("#check'+num+'").click(function() { checkbox'+num+'();});'+
-'function checkbox'+num+'() {  '+
- 'var checknum = "'+todo.group+'";'+
- 'var checkonoff="'+todo.isCheck+'";'+
-// 'alert("asdd");'+
-'$.post("/todo", {checkcheck: checknum,checkbool:checkonoff}, function(res){console.log(res);'+
-'getList();'+
-'});'+
-'}</script>');
-*/
-}else{
-	parsent=0;
-	parsent2=0;
-	
-	$list.append('<div class="deleat"><input id="deleatButton'+num+'" type="button" value="×"></div>'+
-
-	'<div class="todotodo"><div class="linktodo'+num+'"><a href="/task" style="text-decoration:none;"><div class="title">'+todo.group+'</div>'+
-	'<div class="todo"><div><div class="par">達成率'+parsent2+'％</div><div class=parbar><img src="images/lightgreen.jpg" width="'+parsent2+'%" height="30"></div></div>'+
-	'<div>ToDoはありません'+' 作成日：' +creyear+'/'+cremonth+'/'+credate +'</div>'+
-	'</div></div></div>');
-
-
-$list.append('<script>$(".linktodo'+num+'").click(function() { editbutton'+num+'(); });'+'function editbutton'+num+'() { sessionStorage.setItem("tasknum","'+todo.group+'");}</script>');
-$list.append('<script>$("#deleatButton'+num+'").click(function() { deleatbutton'+num+'();});'+
-'function deleatbutton'+num+'() {  '+
- 'var deleatss = "'+todo.group+'";'+
-'$.post("/todo", {groupname2: deleatss}, function(res){console.log(res);'+
-'getList();'+
-'});'+
-'}</script>');
-	
-	}
-	
-num+=1;
-
-
-
-		}
-	
-		/*
-		totalcount+=1;
-		
-		if(todo.isCheck="true"){
-		count+=1;
-		}
-		$list.append('<div>'+totalcount+'個中'+count+'個クリア</div>');
-		*/
-      });
-      // 一覧を表示する
-      $list.fadeIn();
-    });
-  });
-
-
-}
-
-// フォームに入力されたToDoを追加する
-function postList(){
-
-
-  // フォームに入力された値を取得
-  var groupname = $('#text').val();
-  //var limitDate = new Date($('#limit').val());
-var grouping;
-  //入力項目を空にする
-  $('#text').val('');
- // $('#limit').val('');
-
-  // /todoにPOSTアクセスする
-  $.post('/todo', {group: groupname,grouping: grouping}, function(res){
-    console.log(res);
-    //再度表示する
-	document.getElementById("todocom").innerHTML = "新しいToDoが追加されました"+'<br>';
-
+$(function() {
     getList();
-  });
-}
+});
+// フォームを送信ボタンを押すと、ToDoを追加して再表示する。
+$('#form').submit(function() {
+    postList();
+    return false;
+});
+// ToDo一覧を取得して表示する
+function getList() {
+        var smp;
+        var num;
+        num = 0;
+        // すでに表示されている一覧を非表示にして削除する
+        var $list = $('.list');
+        $list.fadeOut(function() {
+            $list.children().remove();
+            // /todoにGETアクセスする
+            $.get('todo', function(todos) {
+                // 取得したToDoを追加していく
+                $.each(todos, function(index, todo) {
+                    //タイトルにすTodoを表示
+                    if (todo.isTitles == true) {
+                        var create = new Date(todo.createdDate);
+                        var credate = create.getDate();
+                        var cremonth = create.getMonth() +
+                            1;
+                        var creyear = create.getFullYear();
+                        var limit = new Date(todo.limitDate);
+                        var mydate = limit.getDate();
+                        var mymonth = limit.getMonth() +
+                            1;
+                        var myyear = limit.getFullYear();
+                        if (todo.taskcount != 0) {
+                            var parsent = todo.finished /
+                                todo.taskcount * 100;
+                            var parsent2 = Math.round(
+                                parsent);
+                            var setgroup;
+                            setgroup = 'aa';
+                            $list.append(
+                                '<div class="deleat"><input id="deleatButton' +
+                                num +
+                                '" type="button" value="×"></div>' +
+                                '<div class="todotodo"><div class="linktodo' +
+                                num +
+                                '"><a href="/task" style="text-decoration:none;"><div class="title">' +
+                                todo.group +
+                                '</div>' +
+                                '<div class="todo"><div><div class="par">達成率' +
+                                parsent2 +
+                                '％</div><div class=parbar><img src="images/lightgreen.jpg" width="' +
+                                parsent2 +
+                                '%" height="30"></div></div>' +
+                                '<div>' + todo.taskcount +
+                                '個中' + todo.finished +
+                                '個チェック済み' +
+                                //	'<input type="checkbox" id="check'+num+'" ' + (todo.isCheck ? 'checked' : '') + '>'+
+                                ' 作成日：' + creyear +
+                                '/' + cremonth +
+                                '/' + credate +
+                                '  期限：' + myyear +
+                                '/' + mymonth + '/' +
+                                mydate + '</div>' +
+                                //$list.append('<script>$("#check").click(function(){checkbox1();});function checkbox1(){ $.get("/todo", function(todos){ $.each(todos, function(index, todo){todo.isCheck="true";});});}<script>');
+                                //'<div><a href="/task"><input id="linkButton'+num+'" type="button" value="詳細"></a></div>'+
+                                '</div></div></div>'
+                            );
+                            $list.append(
+                                '<script>$(".linktodo' +
+                                num +
+                                '").click(function() { editbutton' +
+                                num + '(); });' +
+                                'function editbutton' +
+                                num +
+                                '() { sessionStorage.setItem("tasknum","' +
+                                todo.group +
+                                '");}</script>');
+                            $list.append(
+                                '<script>$("#deleatButton' +
+                                num +
+                                '").click(function() { deleatbutton' +
+                                num + '();});' +
+                                'function deleatbutton' +
+                                num + '() {  ' +
+                                'var deleatss = "' +
+                                todo.group + '";' +
+                                // 'alert("asdd");'+
+                                '$.post("/todo", {groupname2: deleatss}, function(res){console.log(res);' +
+                                'getList();' +
+                                '});' +
+                                '}</script>');
+                        } else {
+                            parsent = 0;
+                            parsent2 = 0;
+                            $list.append(
+                                '<div class="deleat"><input id="deleatButton' +
+                                num +
+                                '" type="button" value="×"></div>' +
+                                '<div class="todotodo"><div class="linktodo' +
+                                num +
+                                '"><a href="/task" style="text-decoration:none;"><div class="title">' +
+                                todo.group +
+                                '</div>' +
+                                '<div class="todo"><div><div class="par">達成率' +
+                                parsent2 +
+                                '％</div><div class=parbar><img src="images/lightgreen.jpg" width="' +
+                                parsent2 +
+                                '%" height="30"></div></div>' +
+                                '<div>ToDoが作成されていません' +
+                                ' 作成日：' + creyear +
+                                '/' + cremonth +
+                                '/' + credate +
+                                '</div>' +
+                                '</div></div></div>'
+                            );
+                            $list.append(
+                                '<script>$(".linktodo' +
+                                num +
+                                '").click(function() { editbutton' +
+                                num + '(); });' +
+                                'function editbutton' +
+                                num +
+                                '() { sessionStorage.setItem("tasknum","' +
+                                todo.group +
+                                '");}</script>');
+                            $list.append(
+                                '<script>$("#deleatButton' +
+                                num +
+                                '").click(function() { deleatbutton' +
+                                num + '();});' +
+                                'function deleatbutton' +
+                                num + '() {  ' +
+                                'var deleatss = "' +
+                                todo.group + '";' +
+                                '$.post("/todo", {groupname2: deleatss}, function(res){console.log(res);' +
+                                'getList();' +
+                                '});' +
+                                '}</script>');
+                        }
+                        num += 1;
+                    }
+                });
+                // 一覧を表示する
+                $list.fadeIn();
+            });
+        });
+    }
+    // フォームに入力されたToDoを追加する
 
-//if(db.todo.count()>=3){
-//	alert(aa)
+function postList() {
+    // フォームに入力された値を取得
+    var groupname = $('#text').val();
+    var grouping;
 	
-//	}
+	
+	
+	var notword;
+		notword=">";
+		
+		var notword2;
+		notword2="<";
+		
+		var notword3;
+		notword3="&"
+	
+	    $('#text').val('');
+
+	
+	if(groupname.indexOf(notword) !=　-1){
+		 //alert("1文字以上入力してください");
+		 document.getElementById("todocom").innerHTML ="<div class='errtext'><>&の使用は控えてください" + '<br></div>';
+		 return;
+		}else if(groupname.indexOf(notword2) !=　-1){
+		 document.getElementById("todocom").innerHTML ="<div class='errtext'><>&の使用は控えてください" + '<br></div>';
+		 return;
+		}else if(groupname.indexOf(notword3) !=　-1) {
+		 document.getElementById("todocom").innerHTML ="<div class='errtext'><>&の使用は控えてください" + '<br></div>';
+		 return;
+}else if(groupname==""){
+		 document.getElementById("todocom").innerHTML ="<div class='errtext'>テキストフォームに何も書かれていません" + '<br></div>';
+		 return;
+
+
+}
+	
+	
+	
+	
+	
+    //入力項目を空にする
+    // /todoにPOSTアクセスする
+    $.post('/todo', {
+        group: groupname,
+        grouping: grouping
+    }, function(res) {
+        console.log(res);
+        //再度表示する
+        document.getElementById("todocom").innerHTML =
+            "新しいリストが追加されました" + '<br>';
+        getList();
+    });
+}
